d0112053194e62a850f43997a04c192f931d066e
Pooja Kate <82284439+pooja-kate@users.noreply.github.com>
2023-08-07 15:34:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    const board = document.getElementById("board");
+
+    for (let i = 0; i < 800; i++) {
+        const box = document.createElement("div");
+        box.classList.add("box");
+        board.appendChild(box);
+    }
+});
